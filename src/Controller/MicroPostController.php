f3d53d2410e6a3dcb1ed6c2e9936629458bb8468
Vitaly Melnikov <vmelnikov@bk.ru>
2018-09-01 12:12:01 +0300

    59 lesson

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -76,10 +76,22 @@ class MicroPostController
      * @Route("/", name="micro_post_index")
      */
     public
-    function index(): Response
+    function index(TokenStorageInterface $tokenStorage): Response
     {
+        $currentUser = $tokenStorage->getToken()->getUser();
+
+        if ($currentUser instanceof User) {
+            $posts = $this->microPostRepository->findAllByUsers($currentUser->getFollowing());
+        } else {
+            $posts = $this->microPostRepository->findBy(
+                [],
+                ['time' => 'DESC']
+            );
+
+        }
+
         $html = $this->twig->render('micro-post/index.html.twig', [
-            'posts' => $this->microPostRepository->findBy([], ['time' => 'DESC']),
+            'posts' => $posts,
         ]);
 
         return new Response($html);
