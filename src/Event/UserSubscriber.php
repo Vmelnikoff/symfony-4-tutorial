189cc51c1de5b9707040c40716db057b4b147e5b
Vitaly Melnikov <vmelnikov@bk.ru>
2018-09-04 12:30:33 +0300

    79 lesson

--- src/Event/UserSubscriber.php
+++ src/Event/UserSubscriber.php
@@ -6,6 +6,21 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var \Swift_Mailer
+     */
+    private $mailer;
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -15,5 +30,17 @@ class UserSubscriber implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisterEvent $event)
     {
+        $body = $this->twig->render('email/registration.html.twig', [
+            'user' => $event->getRegistredUser(),
+        ]);
+
+        $message = (new \Swift_Message())
+            ->setSubject('Welcome to Micro-Post App!')
+            ->setFrom('micropost@micropost.com')
+            ->setTo($event->getRegistredUser()->getEmail())
+            ->setBody($body, 'text/html')
+        ;
+
+        $this->mailer->send($message);
     }
 }
