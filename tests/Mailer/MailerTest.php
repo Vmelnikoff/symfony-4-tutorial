1aa9e9d734a67713bd2bf9ac6ad0a4741b68c935
Vitaly Melnikov <vmelnikov@bk.ru>
2018-09-06 16:13:31 +0300

    96 lesson

--- tests/Mailer/MailerTest.php
+++ tests/Mailer/MailerTest.php
@@ -5,7 +5,6 @@ namespace App\Tests\Mailer;
 use App\Entity\User;
 use App\Mailer\Mailer;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
-use Symfony\Bundle\TwigBundle\DependencyInjection\TwigExtension;
 
 class MailerTest extends TestCase
 {
@@ -19,14 +18,39 @@ class MailerTest extends TestCase
                             ->disableOriginalConstructor()
                             ->getMock()
         ;
+        $swiftMailer->expects($this->once())
+                    ->method('send')
+                    ->with($this->callback(function ($subject) {
+                        $messageStr = (string)$subject;
+//            /** @var \Swift_Message $messageStr */
+//            $messageStr = $subject;
+//            dump($messageStr->toString());
+//            dump($messageStr);
+
+                        return strpos($messageStr, 'Subject: Welcome to Micro-Post App!') !== false
+                            && strpos($messageStr, 'From: me@domain.com') !== false
+                            && strpos($messageStr, 'To: john@doe.com') !== false
+                            && strpos($messageStr, 'Content-Type: text/html; charset=utf-8') !== false
+                            && strpos($messageStr, 'This is message string') !== false
+                            ;
+                    }))
+        ;
+
 //        $twig = $this->createMock(\Twig_Environment::class);
         $twig = $this->getMockBuilder(\Twig_Environment::class)
-                            ->disableOriginalConstructor()
-                            ->getMock()
+                     ->disableOriginalConstructor()
+                     ->getMock()
+        ;
+        $twig->expects($this->once())
+             ->method('render')
+             ->with('email/registration.html.twig',
+                 [
+                     'user' => $user,
+                 ])
+            ->willReturn('This is message string')
         ;
 
         $mailer = new Mailer($swiftMailer, $twig, 'me@domain.com');
         $mailer->sendConfirmationEmail($user);
     }
-
 }
